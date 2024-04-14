06f4f6a09d9046deee49c0b7b3deac5c67228272
NEXUS-SPACE <120999573+YouTube-TW-MAIN@users.noreply.github.com>
2024-04-14 10:14:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Ganti dengan token bot Telegram Anda
-const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
+const token = '6743529860:AAHI80luSa-4F7eMe-w-1TLG2_DtHqFv6tk';
 
 // Inisialisasi bot dengan token
 const bot = new TelegramBot(token, { polling: true });
